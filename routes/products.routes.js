94ef2682c98cb3257ffadae9de7d3b5b5bc3b97c
Khaled Abdou <khalid.elsehsah@gmail.com>
2023-04-29 20:38:23 +0300

    test for port issue

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -2,23 +2,27 @@ const Product = require('../models/Product.model')
 const User = require('../models/User.model')
 const router = require('express').Router()
 
-// Display product form : 
+// Display product form: 
 router.get('/create', (req, res) => {
     res.render('create-product')
 })
-// Create the product :
+// Create the product:
 router.post('/create', (req, res) => {
     Product.create(req.body)
-    // console.log(req.body)
     .then(() => { res.redirect('/products/products') })
     .catch((error) => { res.render('create-product', {errorMessage: 'Something is wrong plz fill the form again' }) })
 })
 // Display all products:
 router.get('/products', (req, res) => {
     Product.find()
-    .then((allProducts) => { console.log(allProducts)
-    res.render('products', {allProducts})})
+    .then((allProducts) => { res.render('products', {allProducts}) })
     .catch((error) => res.render('Error', {errorMessage: 'Can not get data from database, sorry'}))
     })
+// Edit the product:
+router.get('/edit/:productId', async(req, res) => {
+    const specificProduct = await Product.findById(req.params.productId)
+    console.log("here is a specific", specificProduct)
+    res.render('edit-product', specificProduct)
+})
 
 module.exports = router;
